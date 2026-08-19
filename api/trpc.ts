b48c355e686882fb2745414b58b6b72da3c8826d
hu8181id <hu8181id@gmail.com>
2026-08-19 03:36:29 +0000

    Fix import statement for createFetchContext

--- api/trpc.ts
+++ api/trpc.ts
@@ -1,6 +1,6 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "../server/routers.js";
-import { createFetchContext } from "../server/_core/context";
+import { createFetchContext } from "../server/_core/context.js";
 const endpoint = "/api/trpc";
 export default function handler(request: Request): Promise<Response> {
   return fetchRequestHandler({
